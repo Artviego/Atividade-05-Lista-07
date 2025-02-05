9c20db6bf85aca3bf769d86d20d83942f9e1c64d
Arthur <168754906+Artviego@users.noreply.github.com>
2025-02-05 20:53:03 -0300

    Add files via upload

---
+++ b/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+
+        Console.Write("Insira um número inteiro: ");
+        string input = Console.ReadLine();
+        int numero;
+
+        //Converte a entrada de dados para um inteiro
+        if (int.TryParse(input, out numero))
+        {
+            Console.WriteLine("Tabuada de {0} até o número 10: ", numero);
+
+
+            //Inicializa a variável de controle para o loop
+            int i = 1;
+
+            //Loop para exibir a tabuada
+            while (i <= 10)
+            {
+                Console.WriteLine(numero * i);
+
+                i++;
+            }
+           
+
+        }
+        else
+        {
+            Console.WriteLine("Por favor, insira um número inteiro válido.");
+
+        }
+
+        Console.ReadKey(true);
+    }
+}
