fbd9d668f1aae521b23311b86e24827e6785014f
Arthur <168754906+Artviego@users.noreply.github.com>
2025-02-07 13:46:27 -0300

    commit Letra f)

---
+++ f/f/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Insira um número inteiro positivo: ");
+        string input = Console.ReadLine();
+        int numero;
+
+        if (int.TryParse(input, out numero))
+        {
+            Console.WriteLine("Números pares entre 0 e {0}: ", numero);
+
+            int i = 0;
+
+            // Exibe todos os números pares entre 0 e o número inserido pelo usuário
+            do
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+                i++;
+
+            } while (i <= numero);
+
+        }
+        else
+        {
+            Console.WriteLine("Por favor, insira um número positivo válido.");
+        }
+
+        Console.ReadKey(true);
+    }
+}
