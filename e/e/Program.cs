1ac1a1f335f5071ba0d52c6e47fa7fcb31bc8a4b
Arthur <168754906+Artviego@users.noreply.github.com>
2025-02-07 13:45:50 -0300

    commit Letra e)

---
+++ e/e/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        int soma = 0;
+        int numero;
+
+        Console.WriteLine("Digite números inteiros, quando um número negativo for inserido, o programa exibirá a soma dos números positivos. ");
+
+        while (true)
+        {
+            Console.Write("Digite um número: ");
+            numero = int.Parse(Console.ReadLine());
+
+            if (numero < 0)
+            {
+                break;
+            }
+            else
+            {
+                soma += numero;
+            }
+
+        }
+
+        Console.WriteLine("\nA soma dos números inseridos, excluindo o número negativo, é: {0}", soma);
+        Console.ReadKey(true);
+    }
+}
