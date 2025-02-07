7367628cb02a468259cd4dc636896484a480fcc1
Arthur <168754906+Artviego@users.noreply.github.com>
2025-02-07 13:06:21 -0300

    commit Letra c)

---
+++ c/c/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class Program
+{
+
+    public static void Main()
+    {
+        int i = 0;
+        int soma = 0;
+        string input;
+        int numero;
+
+        Console.WriteLine("Digite uma sequência de 4 números inteiros. ");
+
+        while (i < 5)
+        {
+            Console.Write("Digite um número: ");
+            input = Console.ReadLine();
+
+            // Tenta converter a entrada para um número inteiro
+            if (int.TryParse(input, out numero))
+            {
+                soma += numero;
+
+            }
+            else
+            {
+                Console.WriteLine("Entrada inválida! Por favor, digite um número inteiro");
+
+            }
+
+            i++;
+        }
+
+        Console.WriteLine("\nA soma dos números inseridos é: {0}", soma);
+        Console.ReadKey(true);
+    }
+}
